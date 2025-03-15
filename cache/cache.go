f269eaf62804bd4bead563747250649e1c397583
Andres-cmk <ramirezmontanaandresfelipe@gmail.com>
2025-03-14 20:39:36 -0500

    se agrega metodos de cache

--- cache/cache.go
+++ cache/cache.go
@@ -1,1 +1,58 @@
 package cache
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+type CacheObject struct {
+	StatusCode   int
+	Headers      map[string]string
+	ResponseBody string
+	Created      time.Time
+	ExpireAt     time.Time
+}
+
+type Cache struct {
+	data map[string]*CacheObject
+	mu   sync.RWMutex
+}
+
+// NewCache crea una instancia de caché
+
+func NewCache() *Cache {
+	return &Cache{
+		data: make(map[string]*CacheObject),
+	}
+}
+
+func (c *Cache) Store(url *string, value *CacheObject, duration time.Duration) bool {
+	if value == nil {
+		return false
+	}
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.data[*url] = value
+	return true
+}
+
+func (c *Cache) ClearAll() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.data = make(map[string]*CacheObject)
+	log.Print("Clearing all cache objects...")
+}
+
+func (c *Cache) Get(url string) (*CacheObject, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	cacheObject, found := c.data[url]
+	if !found || time.Now().After(cacheObject.ExpireAt) {
+		return nil, false // No encontrado o expirado
+	}
+
+	return cacheObject, true
+}
--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"chaching-proxy/cache"
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// inputs por la linea de comandos.
+	port := flag.String("port", "", "-port")
+	urlTarget := flag.String("origin", "", "-url")
+	clearCache := flag.Bool("clear-cache", false, "-clear-cache")
+	flag.Parse()
+	StartServer(port, clearCache, urlTarget)
+}
 
-	log.Print("Listening on port 8080...")
-	_ = http.ListenAndServe(":8080", nil)
-
+func StartServer(port *string, clearCache *bool, target *string) {
+	newCache := cache.NewCache()
+	if *clearCache == true {
+		newCache.ClearAll()
+	}
+	log.Print("Listening on port " + *port + "...")
+	_ = http.ListenAndServe(":"+*port, nil)
 }
